81a146b37f26af661dbce7467fe7e750e57116ca
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2022-01-23 20:03:15 -0500

    Update lists
    Lists updated to include one capital letter and more options to avoid duplicate passwords.

--- pw_gen.py
+++ pw_gen.py
@@ -7,24 +7,24 @@
 # rand_var_str : Convert list to string.
 
 
-
+import time
 import string
 import random
 
 # First word list
-words_1 = "blue teal spot feel drag work sudo snow soda nope goth heat goat arch ouch free lock luck pick pond corn cube fire wind pink"
+words_1 = "blue teal spot feel drag work sudo snow soda nope goth heat goat arch ouch free lock luck pick pond corn cube fire wind pink rune push posh grey zeal jazz kill join"
 word_list_1 = words_1.split()
 rand_word_1 = random.choices(word_list_1)
 rand_word_str_1 = " ".join(rand_word_1)
 
 # Second word list
-words_2 = "first split crash grown throw enter flame water earth trash green violet"
+words_2 = "First Split Crash Grown Throw Enter Flame Water Earth Trash Green Violet Cloud Rhino Tiger Dread Death Third Egypt Claim Grass Goose Moose"
 word_list_2 = words_2.split()
 rand_word_2 = random.choices(word_list_2)
 rand_word_str_2 = " ".join(rand_word_2)
 
 # Integer list
-int = "1 2 3 4 5 6 7 8 9"
+int = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 int_list = int.split()
 rand_int = random.choices(int_list)
 rand_int_str = " ".join(rand_int)
@@ -38,3 +38,5 @@ rand_cha_str = " ".join(rand_cha)
 
 
 print(f"Your password is: {rand_word_str_1}{rand_word_str_2}{rand_int_str}{rand_cha_str}")
+time.sleep(2)
+print("Your new password will not be saved or displayed after the program is closed. 
